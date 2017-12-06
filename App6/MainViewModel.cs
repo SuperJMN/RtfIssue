312f2d46c2ee1b8cd0544ea1711cc4abeac5c303
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-12-06 13:25:57 +0100

    Add project files.

---
+++ App6/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using ReactiveUI;
+
+namespace App6
+{
+	public class MainViewModel : ReactiveObject
+	{
+		public TextModel TextModel { get; } = new TextModel();
+
+		public MainViewModel()
+		{
+			SaveCommand = ReactiveCommand.CreateFromTask(GetValue);
+		}
+
+		private async Task GetValue()
+		{
+			var saveFilePicker = new FileSavePicker()
+			{
+				CommitButtonText = "Save",
+				FileTypeChoices = { { "XML", new List<string>() { ".xml" } } },
+			};
+
+			var file = await saveFilePicker.PickSaveFileAsync();
+
+			using (var output = await file.OpenStreamForWriteAsync())
+			{
+				var serializer = new ConfigurationContainer().Create();
+				serializer.Serialize(output, TextModel);
+			}
+		}
+
+		public ReactiveCommand<Unit, Unit> SaveCommand { get; set; }
+	}
+
+	public class TextModel : ReactiveObject
+	{
+		private string text;
+
+		public string Text
+		{
+			get => text;
+			set => this.RaiseAndSetIfChanged(ref text, value);
+		}
+	}
+}
---
+++ App6/RichEditBoxEx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reactive.Linq;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App6
+{
+    public class RichEditBoxEx : RichEditBox
+    {
+        public RichEditBoxEx()
+        {
+            Observable
+                .FromEventPattern<RoutedEventHandler, RoutedEventArgs>(x => this.LostFocus += x, x => this.LostFocus -= x)
+                .Select(ea =>
+                {
+                    Document.GetText(TextGetOptions.FormatRtf, out var t);
+                    return t;
+                })
+                .DistinctUntilChanged()
+                .Subscribe(s => Text = s);
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(RichEditBoxEx), new PropertyMetadata(default(string), OnTextChanged));
+
+        private static void OnTextChanged(DependencyObject target, DependencyPropertyChangedEventArgs args)
+        {
+            var rte = (RichEditBoxEx) target;
+            var o = (string) args.OldValue;
+            var n = (string) args.NewValue;
+
+            rte.OnTextChanged(o, n);
+        }
+
+        private void OnTextChanged(string oldValue, string newValue)
+        {
+            Document.SetText(TextSetOptions.FormatRtf | TextSetOptions.CheckTextLimit | TextSetOptions.ApplyRtfDocumentDefaults, newValue);
+        }
+
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
